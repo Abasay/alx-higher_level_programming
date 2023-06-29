70f6bdbedc711cb792c8cf3ba28341e0592267aa
Abasay <abdulsalamasheem@gmail.com>
2023-06-29 16:58:48 -0700

    task 4 and 5

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS $1 -i | grep -i Allow | awk '{print $2, $3, $4}'
+curl -sX OPTIONS $1 -i | grep -i Allow | awk -F" " '{ for (i=2; i<=NF; i++) printf $i""S; print ""}'
