479dd1a9f48f3704f60a35adec36757008496a8a
Abasay <abdulsalamasheem@gmail.com>
2023-06-29 17:16:38 -0700

    fixing the bug in task 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS $1 -i | grep -i Allow | awk -F" " '{ for (i=2; i<=NF; i++) printf $i""S; print ""}'
+curl -sX OPTIONS $1 -i | grep -i Allow | awk '{$1=""; print substr($0,2)}'
