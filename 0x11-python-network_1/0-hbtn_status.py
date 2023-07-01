cc66b0ad8031cf6d504063698f005b1d38a78be3
Abasay <abdulsalamasheem@gmail.com>
2023-06-30 21:05:35 -0700

    the first 3 task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
+
+import urllib.request
+
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as res:
+    data = res.read()
+
+print(f"Body response:\n\t- type: {type(data)}\n\t- content: {data}\n\t- utf8 content: {data.decode('utf-8')}")
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)"""
+
+import urllib.parse
+import sys
+import urllib.request
+
+email = {'email' : sys.argv[2]}
+
+data = urllib.parse.urlencode(email)
+data = data.encode('ascii')
+req = urllib.request.Request(sys.argv[1], data)
+with urllib.request.urlopen(req) as res:
+    the_page = res.read()
+print(the_page)
