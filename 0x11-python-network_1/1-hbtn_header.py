383a2759996b1c4ed8dcd2df8a1d589e14bf8104
Abasay <abdulsalamasheem@gmail.com>
2023-07-01 20:19:51 -0700

    the first 2 task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response."""
+"""
+a Python script that takes in a URL, sends a request to the URL
+"""
 
 import urllib.request
 import sys
 
 req = urllib.request.Request(sys.argv[1])
 with urllib.request.urlopen(req) as res:
-    print(res.headers['X-Request-Id'])
+    data = res.getheader('X-Request-Id')
+print(data)
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,10 +6,9 @@ import sys
 import urllib.request
 
 email = {'email' : sys.argv[2]}
-
 data = urllib.parse.urlencode(email)
 data = data.encode('ascii')
 req = urllib.request.Request(sys.argv[1], data)
 with urllib.request.urlopen(req) as res:
-    the_page = res.read()
+    the_page = res.read().decode('utf-8')
 print(the_page)
