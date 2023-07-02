fc863e17ef5eadc1ebbb99ab4f274d77947d246d
Abasay <abdulsalamasheem@gmail.com>
2023-07-01 22:29:52 -0700

    fixing the pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,4 +7,6 @@ req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as res:
     data = res.read()
 print("Body response:")
-print(f"\t- type: {type(data)}\n\t- content: {data}\n\t- utf8 content: {data.decode('utf-8')}")
+print(f"\t- type: {type(data)}")
+print(f"\t- content: {data}")
+print(f"\t- utf8 content: {data.decode('utf-8')}")
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,11 +5,13 @@ a Python script that takes in a URL, sends a request to the URL
 import urllib.request
 import sys
 
+
 def main(url):
     """ function to print a header of a url """
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as res:
         print(res.getheader('X-Request-Id'))
+
 
 if __name__ == "__main__":
     main(sys.argv[1])
