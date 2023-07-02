b7edc9a066decdb8452b2c6089fe09ec872c7ec9
Abasay <abdulsalamasheem@gmail.com>
2023-07-01 20:27:03 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,11 +2,9 @@
 """
 a Python script that takes in a URL, sends a request to the URL
 """
-
 import urllib.request
 import sys
 
 req = urllib.request.Request(sys.argv[1])
 with urllib.request.urlopen(req) as res:
-    data = res.getheader('X-Request-Id')
-print(data)
+    print(res.getheader('X-Request-Id'))
