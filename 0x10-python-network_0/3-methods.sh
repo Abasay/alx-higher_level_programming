80f8f6f0d3905eba980725f907ab7c662e68caf5
Abasay <abdulsalamasheem@gmail.com>
2023-07-02 20:08:24 -0700

    fixing the bugs

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS $1 -i | grep -i Allow | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}'
+curl -sI "$1" | grep -i "Allow" | cut -d " " -f 2-
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,12 +7,20 @@ def find_peak(list_of_integers):
      Args:
           list_of_integers (int) : list of integers to check
     """
-    peak = 0
-    
-    if len(list_of_integers) == 0:
+     if list_of_integers is None or list_of_integers == []:
         return None
-
-    for i in range(0, len(list_of_integers)):
-        if list_of_integers[i] > peak:
-            peak = list_of_integers[i]
-    return peak
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
