1769f259a88869044eae598e36fc3310635c7567
Abasay <abdulsalamasheem@gmail.com>
2023-07-04 21:25:13 -0700

    merger

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS $1 -i | grep -i Allow | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}'
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+
