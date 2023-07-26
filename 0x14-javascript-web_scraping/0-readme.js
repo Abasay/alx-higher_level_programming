ece33d61c42aeaa7373558d56041cfc3618f3930
Abasay <abdulsalamasheem@gmail.com>
2023-07-26 11:19:46 -0700

    task 1

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const process = require('process');
+const args = process.argv;
+
+fs.readFile(args[2], encoding='utf-8', (err, data) => {
+    if (err) {
+	console.log(err);
+    }
+    else {
+	console.log(data);
+    }
+})
